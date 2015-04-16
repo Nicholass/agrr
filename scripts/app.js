ad31ceb58a28ae2c06910813205d51611134b4d4
Mykola Korobko <muscimol@wnb.com.ua>
2015-04-16 17:46:04 +0300

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+(function() {
+	var app = angular.module('agrr', []);
+
+	app.controller('agrrController', function() {
+		this.items = places;
+	});
+
+	app.controller('rowController', function() {
+		this.type = 1;
+	});
+
+	var places = [
+		{
+			id: 1,
+			submittedDate: 1212121212121,
+			title: 'ДШС в лесу',
+			coordinates: [5.9817263, 3.23412],
+			description: 'глубоко в лесу есть герма блаблабла',
+			type: "Дренаж",
+			submittedBy: "ACIS",
+			visited: false
+		},
+		{
+			id: 2,
+			submittedDate: 1212321212121,
+			title: 'Коллектор в Буче',
+			coordinates: [5.9817263, 3.23412],
+			description: 'глубоко в лесу есть герма блаблабла',
+			type: "Ливневый коллектор",
+			submittedBy: "ACIS",
+			visited: true,
+			visitDate: 123123123123,
+			visitComments: ""
+		}
+
+	]
+})();
