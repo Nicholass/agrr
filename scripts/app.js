c9c2dec9df76d642b1c7bdc21c181aff59ae81c4
Mykola Korobko <muscimol@wnb.com.ua>
2015-04-18 04:13:10 +0300

    html redone table -> divs; moreInfo div shown on Click; sorBy navigation;
    fixed circles CSS

--- scripts/app.js
+++ scripts/app.js
@@ -1,38 +1,58 @@
 (function() {
 	var app = angular.module('agrr', []);
 
-	app.controller('agrrController', function() {
+	app.controller('tableController', function() {
 		this.items = places;
-		// TODO highlght anchor
+		this.currentPlace = 0;
+		this.orderingField = "id";
+
+		this.navigate = function(id) {
+			this.currentPlace = this.currentPlace != id? id : 0;
+		};
+
+		this.checkShow = function(id) {
+			return id == this.currentPlace;
+		};
+
+		this.sortBy = function(field) {
+			this.orderingField = field;
+		};
 	});
 
 	app.controller('rowController', function() {
 		this.backgroundColors = styles;
-		this.color = '#' + this.backgroundColors["Неизвестно/Другое"];
+		this.color = this.backgroundColors["Неизвестно/Другое"];
 
 		this.setColor = function(placeType) {
 			if (placeType in styles) {
-				this.color = '#' + this.backgroundColors[placeType];
+				this.color = this.backgroundColors[placeType];
 			}
-			return {background: this.color };
+			return this.color;
 		};
 
 	});
 
-	app.controller('theadController', function() {
-		this.sortBy = function(col) {
-			// TODO rootscope sortby; ng-click sortBy()
-		}
+	app.controller('TabController', function(){
+		this.tab = 0;
+
+		this.setTab = function(newValue){
+			this.tab = newValue;
+		};
+
+		this.isSet = function(tabName){
+			return this.tab === tabName;
+		};
 	});
 
+
 	var styles = {
-		"Дренаж": "BDBDBD",
-		"Ливневый коллектор": "99CCFF",
-		"Бытовая канализация": "FFA347",
-		"Коммунальный коллектор": "6CB56C",
-		"Подвал": "FF6699",
-		"Историческое сооружение": "FFF047",
-		"Неизвестно/Другое": "5C5CAD"
+		"Дренаж": "#B0BEC5",
+		"Ливневый коллектор": "#90CAF9",
+		"Бытовая канализация": "#FFCC80",
+		"Коммунальный коллектор": "#81C784",
+		"Подвал": "#F48FB1",
+		"Историческое сооружение": "#FFEB3B",
+		"Неизвестно/Другое": "#CE93D8"
 	};
 
 	var places = [
@@ -40,7 +60,10 @@
 			id: 1,
 			submittedDate: 1212121212121,
 			title: 'ДШС в лесу',
-			coordinates: [5.9817263, 3.23412],
+			coordinates: {
+				'lat': 50.468869,
+				'lon': 3.23412
+			},
 			description: 'глубоко в лесу есть герма блаблабла',
 			type: "Дренаж",
 			submittedBy: "ACIS",
@@ -68,7 +91,7 @@
 			submittedBy: "ACIS",
 			visited: true,
 			visitDate: 123123123123,
-			visitComments: ""
+			visitComments: "Interesting place to see"
 		}
 ,
 		{
@@ -98,7 +121,7 @@
 		}
 ,
 		{
-			id: 2,
+			id: 6,
 			submittedDate: 1212321212121,
 			title: 'Коллектор в Буче',
 			coordinates: [5.9817263, 3.23412],
